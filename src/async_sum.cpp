3ac28f423935daf99af36746e2fb46bc2b888743
Yusuf Etkin KIZILDAĞ <49615906+yusufetkinkizildag@users.noreply.github.com>
2022-10-28 00:50:36 +0300

    Update async_sum.cpp

--- src/async_sum.cpp
+++ src/async_sum.cpp
@@ -8,16 +8,9 @@
 #include <chrono>
 #include <future>
 #include <thread>
+#include <fstream>
 
 
-constexpr static std::array<size_t, 65> VECTOR_LENGTHS{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
-                                                       12, 24, 36, 48, 60, 72, 84, 96, 108,
-                                                       120, 240, 360, 480, 600, 720, 840, 960, 1080,
-                                                       1200, 2400, 3600, 4800, 6000, 7200, 8400, 9600, 10800,
-                                                       12000, 24000, 36000, 48000, 60000, 72000, 84000, 96000, 108000,
-                                                       120000, 240000, 360000, 480000, 600000, 720000, 840000, 960000, 1080000,
-                                                       1200000, 2400000, 3600000, 4800000, 6000000, 7200000, 8400000, 9600000, 10800001};
-// constexpr static std::array<size_t, 12> VECTOR_LENGTHS{1,2,3,4,5,6,7,8,9,10,11,12};
 namespace utility
 {
 
@@ -28,7 +21,7 @@ namespace utility
         auto const result{callback(std::forward<Args>(args)...)};
         auto t2 = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double, std::milli> ms_double = t2 - t1;
-        std::cout << ms_double.count() << "ms\n";
+        // std::cout << ms_double.count() << "ms\n";
         return std::make_pair(result, ms_double.count());
     }
 
@@ -60,6 +53,7 @@ namespace utility
         std::copy(std::cbegin(a) + start, std::cbegin(a) + end - 1 , std::ostream_iterator<C>(std::cout, ", "));
         std::cout << a.at(end) << "]\n";
     }};
+
 } // namespace utility
 
 namespace etkin
@@ -116,8 +110,31 @@ int main(int argc, char const *argv[])
 {
     static auto const processor_count{std::thread::hardware_concurrency()}; // 12 on my system
     std::cout << "processor_count: " <<processor_count << std::endl;
-    std::for_each(std::cbegin(VECTOR_LENGTHS), std::cend(VECTOR_LENGTHS), [](auto const length) noexcept
+    std::ofstream file{"data.csv"};
+    file << "length,async_sum,range_sum\n";
+    for (auto i{12}; i < 12'000'000; i *= 10)
     {
+        for (auto j{1}; j < 9; ++j)
+        {
+            auto const length{i * j};
+            std::cout << "length: " << length << std::endl;
+            std::vector<double> in1(length);
+            std::vector<double> in2(length);
+            std::iota(std::begin(in1), std::end(in1), 0);   // 0,1,2,3,4,5,6,7...n-2, n-1
+            std::iota(std::rbegin(in2), std::rend(in2), 0); // n-1, n-2,n-3... 2,1,0
+            auto const res1{utility::time_function_execution(etkin::async_sum, in1, in2, processor_count)};
+            auto const res2{utility::time_function_execution(etkin::range_sum, std::cref(in1), std::cref(in2), 0, length)};
+            // utility::print_all(res1.first);
+            // utility::print(res2.first);
+            file << length << ',' << res1.second << ',' << res2.second << '\n';
+            std::cout << res1.second << " ms" << std::endl;
+            std::cout << res2.second << " ms" << std::endl;
+            std::cout << std::endl;
+        }
+    }
+    for (auto i{12'000'000}; i < 76'000'000; i += 1'000'000)
+    {
+        auto const length{i};
         std::cout << "length: " << length << std::endl;
         std::vector<double> in1(length);
         std::vector<double> in2(length);
@@ -127,9 +144,11 @@ int main(int argc, char const *argv[])
         auto const res2{utility::time_function_execution(etkin::range_sum, std::cref(in1), std::cref(in2), 0, length)};
         // utility::print_all(res1.first);
         // utility::print(res2.first);
-        std::cout << res1.second << std::endl;
-        std::cout << res2.second << std::endl;
+        file << length << ',' << res1.second << ',' << res2.second << '\n';
+        std::cout << res1.second << " ms" << std::endl;
+        std::cout << res2.second << " ms" << std::endl;
         std::cout << std::endl;
-    });
+    }
+    file.close();
     return 0;
 }
